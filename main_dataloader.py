19466c067614b2e1fa2c22904f45f356d73b2f77
lnconsistent <yx1797@nyu.edu>
2023-12-08 17:41:25 -0500

    Update main_dataloader.py

--- main_dataloader.py
+++ main_dataloader.py
@@ -13,7 +13,7 @@ class JSONDataset(IterableDataset):
     def __iter__(self):
         reader = pd.read_json(self.file_path, lines=True, chunksize=self.chunksize)
         for chunk in reader:
-            print(chunk['text'])
+            print(chunk)
             yield (chunk['text'].values, chunk['label'].values)
 
 
