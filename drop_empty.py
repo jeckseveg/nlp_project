265954a7a26609168b6eea15f41aca8d36dcd8b5
lnconsistent <yx1797@nyu.edu>
2023-12-08 00:44:38 -0500

    Update drop_empty.py

--- drop_empty.py
+++ drop_empty.py
@@ -18,7 +18,7 @@ def main(spark):
         data = spark.read.json(file)
         data.createOrReplaceTempView('data')
         print('Removing null or empty values...')
-        data = data.select([to_null(data.text).alias(data.text)]).na.drop()
+        data = data.select([to_null('text').alias('text')]).na.drop()
         data.show()
         print('Writing data...')
         # data.write.json(file, mode='overwrite')
