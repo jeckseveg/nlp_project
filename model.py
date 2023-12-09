5370374b6e67601d06c0c2cda719cb7f5bcd7e04
lnconsistent <yx1797@nyu.edu>
2023-12-08 20:24:16 -0500

    Update model.py

--- model.py
+++ model.py
@@ -30,8 +30,7 @@ class ToxicClassifier(pl.LightningModule):
 
     def format_output(self, label, output):
         # logic for different datasets
-        print(label)
-        if label.shape[1] == 4: # youtube
+        if np.asarray(label).shape[1] == 4: # youtube
             output = output[:,[0,2,3,5]]
         return output
 
