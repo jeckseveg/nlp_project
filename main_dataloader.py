b2ed3448857983978b87471e97e5b440c5a01719
lnconsistent <yx1797@nyu.edu>
2023-12-09 23:11:16 -0500

    Update main_dataloader.py

--- main_dataloader.py
+++ main_dataloader.py
@@ -53,7 +53,7 @@ class SmallJSONDataset(Dataset):
         # drop rows where label contains nan
         nan_index = []
         for i, row in enumerate(df.itertuples()):
-            print(row)
+            print(row.label)
             if any(each!=each for each in row.label):
                 nan_index.append(i)
         df = df.drop(index=nan_index)
